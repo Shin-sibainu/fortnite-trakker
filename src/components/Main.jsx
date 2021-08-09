89bf049a428421c966e9446d17fa970624e07cfe
Shin-sibainu <shincode0712@gmail.com>
2021-08-09 16:48:39 +0900

    refactaring,but cant

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,9 @@
 import { Cards } from "./card/Cards";
 import React from "react";
 import { Title } from "./title/Title";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@material-ui/core";
-//import { UseFetch } from "./utils/UseFetch";
+import { UseFetch } from "./utils/UseFetch";
 import {
   CircularProgress,
   Input,
@@ -44,111 +44,25 @@ const useStyle = makeStyles((theme) => ({
 }));
 export const Main = () => {
   const classes = useStyle();
-  const [inputText, setInputText] = useState("");
-  const [isPending, setPending] = useState(false);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
+  const [inputText, setInputText] = useState(""); //デフォルトは空だからエラーになる。
+  const [onSubmit, isOnSubmit] = useState(false);
+  //最初にUseFetchは呼んだほうがいいかも。
+  const data = null; //最初に絶対呼ばれる。
+  const isPending = false; //最初に絶対呼ばれる。
+  const error = null; //最初に絶対呼ばれる。
+  const { data, isPending, error } = UseFetch(inputText);
+
+  useEffect(() => {}, [onSubmit]);
 
   const handleChange = (e) => {
     setInputText(e.target.value);
   };
 
   const handleSubmit = (e) => {
-    setError(null);
-    setPending(true);
-    setData(null);
     e.preventDefault();
     console.log(inputText);
-    //fetch here
-    //この情報をカードの方に持っていきたい。
-    fetch(`https://fortnite-api.p.rapidapi.com/stats/${inputText}`, {
-      method: "GET",
-      headers: {
-        "x-rapidapi-key": "5d9f005d9cmsh913c00abd95dcc3p1ef35ejsn4f0c3ce61d03",
-        "x-rapidapi-host": "fortnite-api.p.rapidapi.com",
-      },
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const defalutsoloData = data.lifetime.all.defaultsolo;
-        //所望のデータ群{kdr: 3.53 kills: 797 killsPerMatch: 2.51 matchesplayed: 318 minutesplayed: 2208 placetop1: 92 placetop10: 140 placetop25: 183 playersoutlived: 11022 score: 61114 winrate: 28.93
-        const statues = [
-          {
-            id: 1,
-            name: "kdr",
-            number: defalutsoloData.kdr,
-          },
-          {
-            id: 2,
-            name: "kills",
-            number: defalutsoloData.kills,
-          },
-          {
-            id: 3,
-            name: "killsPerMatch",
-            number: defalutsoloData.killsPerMatch,
-          },
-          {
-            id: 4,
-            name: "matchesplayed",
-            number: defalutsoloData.matchesplayed,
-          },
-          {
-            id: 5,
-            name: "minutesplayed",
-            number: defalutsoloData.minutesplayed,
-          },
-          {
-            id: 6,
-            name: "placetop1",
-            number: defalutsoloData.placetop1,
-          },
-          {
-            id: 7,
-            name: "placetop10",
-            number: defalutsoloData.placetop10,
-          },
-          {
-            id: 8,
-            name: "placetop25",
-            number: defalutsoloData.placetop25,
-          },
-          {
-            id: 9,
-            name: "playersoutlived",
-            number: defalutsoloData.playersoutlived,
-          },
-          {
-            id: 10,
-            name: "score",
-            number: defalutsoloData.score,
-          },
-          {
-            id: 11,
-            name: "winrate",
-            number: defalutsoloData.winrate,
-          },
-        ];
-        setPending(false);
-        setData(statues);
-        setError(null);
-      })
-      .catch((error) => {
-        setPending(false);
-        setData(null);
-        if (error.message === "Cannot read property 'all' of undefined") {
-          setError("The player does not exit.");
-        }
-        if (error.message === "Cannot read property 'kdr' of undefined") {
-          setError("Failed to get data.Try again.");
-        }
-        if (error.message === "Failed to fetch") {
-          setError("Network Error.Please try again.");
-        }
-        console.log(error.message);
-      });
+    //submitbuttonが押されたかどうかを通知する。
+    isOnSubmit(!onSubmit);
   };
 
   return (
--- src/components/utils/UseFetch.js
+++ src/components/utils/UseFetch.js
@@ -1,21 +1,104 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export const UseFetch = (url) => {
+export const UseFetch = (inputText) => {
+  //const [inputText, setInputText] = useState("");
+  //エンターを押したときのだけのテキスト情報をステートで管理する？
+  const [isPending, setPending] = useState(false);
+  const [error, setError] = useState(null);
   const [data, setData] = useState(null);
 
-  fetch(url, {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "5d9f005d9cmsh913c00abd95dcc3p1ef35ejsn4f0c3ce61d03",
-      "x-rapidapi-host": "fortnite-api.p.rapidapi.com",
-    },
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setData(data);
-    });
+  useEffect(() => {
+    setError(null);
+    setPending(true);
+    setData(null);
 
-  return data;
+    fetch(`https://fortnite-api.p.rapidapi.com/stats/${inputText}`, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "5d9f005d9cmsh913c00abd95dcc3p1ef35ejsn4f0c3ce61d03",
+        "x-rapidapi-host": "fortnite-api.p.rapidapi.com",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const defalutsoloData = data.lifetime.all.defaultsolo;
+        const statues = [
+          {
+            id: 1,
+            name: "kdr",
+            number: defalutsoloData.kdr,
+          },
+          {
+            id: 2,
+            name: "kills",
+            number: defalutsoloData.kills,
+          },
+          {
+            id: 3,
+            name: "killsPerMatch",
+            number: defalutsoloData.killsPerMatch,
+          },
+          {
+            id: 4,
+            name: "matchesplayed",
+            number: defalutsoloData.matchesplayed,
+          },
+          {
+            id: 5,
+            name: "minutesplayed",
+            number: defalutsoloData.minutesplayed,
+          },
+          {
+            id: 6,
+            name: "placetop1",
+            number: defalutsoloData.placetop1,
+          },
+          {
+            id: 7,
+            name: "placetop10",
+            number: defalutsoloData.placetop10,
+          },
+          {
+            id: 8,
+            name: "placetop25",
+            number: defalutsoloData.placetop25,
+          },
+          {
+            id: 9,
+            name: "playersoutlived",
+            number: defalutsoloData.playersoutlived,
+          },
+          {
+            id: 10,
+            name: "score",
+            number: defalutsoloData.score,
+          },
+          {
+            id: 11,
+            name: "winrate",
+            number: defalutsoloData.winrate,
+          },
+        ];
+        setPending(false);
+        setData(statues);
+        setError(null);
+      })
+      .catch((error) => {
+        setPending(false);
+        setData(null);
+        if (error.message === "Cannot read property 'all' of undefined") {
+          setError("The player does not exit.");
+        }
+        if (error.message === "Cannot read property 'kdr' of undefined") {
+          setError("Failed to get data.Try again.");
+        }
+        if (error.message === "Failed to fetch") {
+          setError("Network Error.Please try again.");
+        }
+        console.log(error.message);
+      });
+  }, [data]); //submitしたときのテキスト情報が更新されたときにuseEffect内部が実行される。
+  return { error, isPending, data };
 };
